e4438221963e543dee3133109a1867f81888adae
Nabil-Fareed Alikhan <nabil@happykhan.com>
2024-01-30 11:39:11 +0000

    created prepare script

--- prepare.py
+++ prepare.py
@@ -3,8 +3,9 @@ import re
 import subprocess
 import argparse
 
-def build_singularity_containers(config_file_path, image_dir):
-    os.makedirs(image_dir, exist_ok=True)
+def build_singularity_containers(config_file_path, image_dir, pipeline_name):
+    sing_dir = os.path.join(image_dir, pipeline_name)
+    os.makedirs(sing_dir, exist_ok=True)
     container_mentions = []
 
     with open(config_file_path, 'r') as config_file:
@@ -14,15 +15,16 @@ def build_singularity_containers(config_file_path, image_dir):
                 container_mentions.append(matches.group(1))
 
     for mention in container_mentions:
+        command = f'singularity pull {sing_dir} docker://{mention}'
         print(command)
-        command = f'singularity pull {image_dir} docker://{mention}'
         subprocess.run(command, shell=True)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Build Singularity containers from config file')
     parser.add_argument('-c', '--config_file', type=str, help='Path to the config file', default='nextflow.config')
     parser.add_argument('-i', '--image_dir', type=str, help='Path to the directory to store the Singularity images', default='/well/aanensen/shared/singularity/')
+    parser.add_argument('-p', '--pipeline_name', type=str, help='Pipeline name', default='nf-phylotyping')
 
     args = parser.parse_args()
 
-    build_singularity_containers(args.config_file, args.image_dir)
+    build_singularity_containers(args.config_file, args.image_dir, args.pipeline_name)
